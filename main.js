b5a748749d26d957e7a65357c0cd7a236d98e946
Brayand Chamán <brayandmaaz102@gmail.com>
2022-11-10 17:20:31 -0600

    segundo commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const pokemonContainer = document.querySelector(".pokemon-container");
+
+
+let offset = Math.round(Math.random()*809);
+let limit = 5
+
+function fetchPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+        createPokemon(data);
+    });
+}
+
+function fetchPokemons(offset, limit) {
+  for (let i = offset; i <= offset + limit; i++) {
+    fetchPokemon(i);
+  }
+}
+
+function createPokemon(pokemon){
+    const card = document.createElement("div");
+    card.classList.add("pokemon-block");
+
+    const spriteContainer = document.createElement("div");
+    spriteContainer.classList.add("img-cantainer");
+
+    const sprite = document.createElement("img");
+    sprite. src= pokemon.sprites.front_default
+
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement("p");
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+     
+
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = pokemon.name
+
+    card.appendChild(spriteContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    pokemonContainer.appendChild(card);
+}
+fetchPokemons(offset, limit);
